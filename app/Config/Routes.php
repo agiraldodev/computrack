5ea0307e52cb71377bbc5f3ed6446cc30999a371
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-03-20 10:00:07 -0500

    Insertando nuevo dispositivo en la base de datos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,4 +20,6 @@ $routes->group('clientes', static function ($routes) {
 $routes->group('dispositivos', static function ($routes) {
     $routes->get('nuevo', 'DispositivoController::crear');
     $routes->get('obtenerClientes', 'DispositivoController::obtenerClientes');
+    $routes->get('obtenerNombreClientePorCedula', 'DispositivoController::obtenerNombreClientePorCedula');
+    $routes->post('guardar', 'DispositivoController::registrarDispositivo');
 });
